cf0745034bc37b2b61cd0088dd8ac32291a45f56
Mushegh98 <musheghsahakyan98@gmail.com>
2020-05-21 20:10:46 +0400

    Create Class UserStore for save data where user login.

---
+++ ui/src/main/java/com/wireguard/android/util/UserStore.java
@@ -0,0 +1,36 @@
+package com.wireguard.android.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class UserStore {
+    private static UserStore instance;
+    private SharedPreferences sharedPreferences;
+    private static final String USER_SHARED_PREF = "com.wireguard.android.util.bubbleUserSharedPref";
+    private static final String USER_RESPONSE = "com.wireguard.android.util.bubbleUserResponse";
+
+    public static UserStore getInstance(Context context) {
+        if (instance == null) {
+            synchronized (UserStore.class) {
+                if (instance == null) {
+                    instance = new UserStore(context);
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    private UserStore(Context context) {
+        sharedPreferences = context.getSharedPreferences(USER_SHARED_PREF, Context.MODE_PRIVATE);
+    }
+
+    public void setUserResponse(String response) {
+        sharedPreferences.edit().putString(USER_RESPONSE, response).apply();
+    }
+
+    public String getUserResponse() {
+        return sharedPreferences.getString(USER_RESPONSE, "-1");
+    }
+
+}
