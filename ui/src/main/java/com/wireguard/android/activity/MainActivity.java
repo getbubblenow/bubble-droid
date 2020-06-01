0cdf44a06b42506b57d56c7b15c8536ecd09f7ee
Mushegh98 <musheghsahakyan98@gmail.com>
2020-06-01 19:38:35 +0400

    Adds initViews and initListeners in MainActivity.

--- ui/src/main/java/com/wireguard/android/activity/MainActivity.java
+++ ui/src/main/java/com/wireguard/android/activity/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.wireguard.android.R;
 import com.wireguard.android.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
     private MainViewModel mainViewModel;
+    private TextView bubbleStatus;
+    private TextView deviceStatus;
+    private Button connectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +29,29 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+        initUI();
+    }
+
+    private void initUI() {
+        initViews();
+        initListeners();
+    }
+
+    private void initViews() {
+        bubbleStatus = findViewById(R.id.bubbleStatus);
+        deviceStatus = findViewById(R.id.deviceStatus);
+        connectButton = findViewById(R.id.connectButton);
+    }
+
+    private void initListeners() {
+       connectButton.setOnClickListener(new OnClickListener() {
+           @Override public void onClick(final View v) {
+               connect();
+           }
+       });
+    }
+
+    private void connect() {
+
     }
 }
